a0cf33f5d375d03e3de94b299f0d8127dccfc95d
Dinesh Biswal <dinesh.biswal@sonata-software.com>
2021-12-21 11:21:05 +0530

    Updated the files

--- InsuranceData.py
+++ InsuranceData.py
@@ -6,46 +6,47 @@ Created on Thu Dec  9 20:42:50 2021
 @author: dinesh
 """
 
+from typing import Optional
+
 from pydantic import BaseModel
 
 
 class InsuranceData(BaseModel):
-    witnesses:int64
-    incident_state:object
-    incident_city:object
-    incident_location:object
-    incident_hour_of_the_day:int64
-    number_of_vehicles_involved:int64
-    property_damage:object
-    bodily_injuries:int64
-    police_report_available:object
-    incident_severity:object
-    total_claim_amount:int64
-    injury_claim:int64
-    property_claim:int64
-    vehicle_claim:int64
-    auto_make:object
-    auto_model:object
-    auto_year:int64
-    authorities_contacted:object
-    collision_type:object
-    age:int64
-    insured_zip:int64
-    policy_number:int64
-    policy_bind_date:object
-    policy_state:object
-    policy_csl:object
-    policy_deductable:int64
-    policy_annual_premium:float64
-    umbrella_limit:int64
-    insured_sex:object
-    incident_type:object
-    insured_education_level:object
-    insured_occupation:object
-    insured_hobbies:object
-    insured_relationship:object
-    capital-gains:int64
-    capital-loss:int64
-    incident_date:object
-    
-    
+    months_as_customer: int
+    age: int
+    policy_number: int
+    policy_bind_date: str
+    policy_state: str
+    policy_csl: str
+    policy_deductable: float
+    policy_annual_premium: float
+    umbrella_limit: Optional[int] = None
+    insured_zip: Optional[int] = None
+    insured_sex: str
+    insured_education_level: str
+    insured_occupation: str
+    insured_hobbies: str
+    insured_relationship: str
+    capital_gains: Optional[int] = 0
+    capital_loss: Optional[int] = 0
+    incident_date: str
+    incident_type: str
+    collision_type: str
+    incident_severity: str
+    authorities_contacted: str
+    incident_state: str
+    incident_city: str
+    incident_location: str
+    incident_hour_of_the_day: int
+    number_of_vehicles_involved: int
+    property_damage: str
+    bodily_injuries: int
+    witnesses: int
+    police_report_available: str
+    total_claim_amount: int
+    injury_claim: int
+    property_claim: int
+    vehicle_claim: int
+    auto_make: Optional[str] = None
+    auto_model: Optional[str] = None
+    auto_year: Optional[int] = None
--- app.py
+++ app.py
@@ -8,38 +8,53 @@ Created on Thu Dec  9 20:35:28 2021
 
 import uvicorn
 from fastapi import FastAPI
+
 from InsuranceData import InsuranceData
-from Preprocessor import Preprocessor
-import pickle
+
 import pandas as pd
+import pickle
 
 app = FastAPI()
-pickle_in = open("model/insurance_fruad_model.pkl","rb")
-classifier=pickle.load(pickle_in)
+# loading saved models
+classifier = pickle.load(open("model/xgb_best_model.pkl", "rb"))
+preprocessor = pickle.load(open("model/preprocessor_pipeline.pkl", "rb"))
+
 
 @app.get('/')
 def index():
-    return {'message': 'Welcome , '}
+    return {'message': 'Welcome ,to the fraud detection application '}
 
 
 @app.post('/dopredict')
-def predict_insurance_fraud(data:InsuranceData):
+def predict_insurance_fraud(data: InsuranceData):
     data = data.dict()
     df = pd.DataFrame(data, index=[0])
-    
-    df = Preprocessor.preprocess(df)
-    
-    
+
+    # preprocessing data
+    df['policy_bind_year'] = df['policy_bind_date'].str.extract('(\d{4})\-').astype('int')
+
+    df['incident_month'] = df['incident_date'].str.extract('\d{4}\-(\d{2})').astype('int')
+    unreported_cols = df.columns[df.isin(['?']).any()]
+    if unreported_cols.any():
+        df[unreported_cols] = df[unreported_cols].replace('?', 'unreported')
+
+    large_cat = ['policy_bind_date', 'incident_date', 'incident_location',
+                 'insured_zip', 'auto_model', 'policy_number', 'age', 'total_claim_amount']
+    df.drop(large_cat, axis=1, inplace=True)
+    df.reset_index(inplace=True, drop=True)
+    df = preprocessor.transform(df)
+
     prediction = classifier.predict(df)
-    if(prediction[0] > 0.5):
-        prediction ="Fraud"
+    if prediction[0] == 0:
+        prediction = "Not Fraud"
     else:
-        prediction ="Not Fraud"
+        prediction = "Fraud"
     return {
         'prediction': prediction
     }
 
+
 if __name__ == '__main__':
     uvicorn.run(app, host='127.0.0.1', port=8000)
-    
-#uvicorn app:app --reload
+
+# uvicorn app:app --reload
